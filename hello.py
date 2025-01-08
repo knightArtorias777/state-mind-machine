666a5f2d0e0ea064b8b878099fb0536ee34c768c
tcs <gumikufans@qq.com>
2025-01-08 14:28:47 +0800

    Your commit message here

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from state-mind-machine!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ sence/toolkit.py
@@ -0,0 +1,23 @@
+
+def switch_workstate():
+    workspace()
+    return
+
+
+# 重点在于以一个舒服的屏幕布局展开 这是最重要的
+def workspace():
+    open_terminal()  # 终端内部加上翻译和chatgpt
+    open_Browser()  # 自动打开相关的项目文档
+    open_ide()  # code .
+
+
+def open_terminal():
+    pass
+
+
+def open_Browser():
+    pass
+
+
+def open_ide():
+    pass
